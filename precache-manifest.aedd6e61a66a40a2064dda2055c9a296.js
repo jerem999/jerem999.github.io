2517858d14227d2098e0a5c2c556bf5ddda7111b
Jérémy Boisdron <boisdron.jeremy@gmail.coml>
2019-02-12 16:13:59 +0100

    test

---
+++ precache-manifest.aedd6e61a66a40a2064dda2055c9a296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "966909cfbce8cbfda9aa",
+    "url": "/js/chunk-vendors.25747607.js"
+  },
+  {
+    "revision": "d0e5f52369d4ac5ebb40",
+    "url": "/js/app.be9e599c.js"
+  },
+  {
+    "revision": "6dde3fc3fc0072f424e8be8154d2dbe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d0e5f52369d4ac5ebb40",
+    "url": "/css/app.7e7cfee3.css"
+  }
+];
